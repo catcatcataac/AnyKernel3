524c998a6e48b320762e9dd50f503c293b71a419
ゆずねこ <ax3526435@gmail.com>
2025-08-21 05:59:42 +0800

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,45 @@
+properties() { '
+kernel.string=KernelSU by KernelSU Developers
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; }
+
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
+
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    4.1*) ksu_supported=true ;;
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
+
+ui_print "此内核来源于北极星"
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" ] || [ -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot
+    flash_boot
+else
+    dump_boot
+    write_boot
+fi
